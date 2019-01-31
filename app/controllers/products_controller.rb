caaffab773b2e7c164774f12cc7bf2cbdf08ad0c
sbassmed <sebastiancamilomarin@gmail.com>
2019-01-31 16:58:54 -0500

    FALTA MODIFICAR VISTAS MAS PULIDAS CON BOOTSTRAP

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,7 +38,7 @@ class ProductsController < ApplicationController
 
 private
   def product_params
-    params.require(:product).permit(:name, :price)
+    params.require(:product).permit(:name, :price, category_ids:[])
   end
 
 end
