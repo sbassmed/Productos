bf0cafd3666b3662dcc86690aa72d7dfbd369d5b
sbassmed <sebastiancamilomarin@gmail.com>
2019-01-31 01:46:16 -0500

    Corri migracion category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_31_063007) do
+ActiveRecord::Schema.define(version: 2019_01_31_063619) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
