8c37753d5ca3cf6c1ce9f58f89db0bd4ae59f6be
sbassmed <sebastiancamilomarin@gmail.com>
2019-01-31 16:33:57 -0500

    Corri migracion tabla intermedia

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_31_063619) do
+ActiveRecord::Schema.define(version: 2019_01_31_212740) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_products", id: false, force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "category_id"
+    t.integer "products_id"
+    t.index ["category_id"], name: "index_categories_products_on_category_id"
+    t.index ["products_id"], name: "index_categories_products_on_products_id"
   end
 
   create_table "products", force: :cascade do |t|
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  price      :decimal(, )
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
